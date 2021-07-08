59bed90af87b0a723f01d46a4cf3183c04812009
Simone <Simone@DESKTOP-BHGM51H>
2021-07-08 16:16:16 +0200

    soluzione punto 1

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -1,5 +1,24 @@
 package it.polito.tdp.librettovoti.model;
 
+import java.util.*;
+
 public class Libretto {
 
+	private List<Voto> voti;
+	
+	public Libretto(){
+		this.voti=new ArrayList<>();
+	}
+	
+	public void add(Voto v) {
+		this.voti.add(v);
+	}
+	
+	public String toString() {
+		String s="";
+		for(Voto v:this.voti) {
+			s=s+=v.toString()+"\n";
+		}
+		return s;
+	}
 }
--- src/main/java/it/polito/tdp/librettovoti/model/Voto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Voto.java
@@ -1,5 +1,49 @@
 package it.polito.tdp.librettovoti.model;
 
-public class Voto {
+import java.time.LocalDate;
 
+public class Voto {
+	private String nome;
+	private int voto;
+	private LocalDate data;
+	
+	public Voto(String nome, int voto, LocalDate data) {
+		super();
+		this.nome = nome;
+		this.voto = voto;
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Esame " + nome + " superato con " + voto + " il " + data;
+	}
+	
+	
+	
+	
 }
